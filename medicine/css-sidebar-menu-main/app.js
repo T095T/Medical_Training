aa7077a4e0747bcd135eb2fcc425d30d1f17bca0
Pranav Deshpande <72501211+T095T@users.noreply.github.com>
2023-10-29 17:45:45 +0530

    Add files via upload

---
+++ medicine/css-sidebar-menu-main/app.js
@@ -0,0 +1,85 @@
+const shrink_btn = document.querySelector(".shrink-btn");
+const search = document.querySelector(".search");
+const sidebar_links = document.querySelectorAll(".sidebar-links a");
+const active_tab = document.querySelector(".active-tab");
+const shortcuts = document.querySelector(".sidebar-links h4");
+const tooltip_elements = document.querySelectorAll(".tooltip-element");
+
+let activeIndex;
+
+shrink_btn.addEventListener("click", () => {
+  document.body.classList.toggle("shrink");
+  setTimeout(moveActiveTab, 400);
+
+  shrink_btn.classList.add("hovered");
+
+  setTimeout(() => {
+    shrink_btn.classList.remove("hovered");
+  }, 500);
+});
+
+search.addEventListener("click", () => {
+  document.body.classList.remove("shrink");
+  search.lastElementChild.focus();
+});
+
+function moveActiveTab() {
+  let topPosition = activeIndex * 58 + 2.5;
+
+  if (activeIndex > 3) {
+    topPosition += shortcuts.clientHeight;
+  }
+
+  active_tab.style.top = `${topPosition}px`;
+}
+
+function changeLink() {
+  sidebar_links.forEach((sideLink) => sideLink.classList.remove("active"));
+  this.classList.add("active");
+
+  activeIndex = this.dataset.active;
+
+  moveActiveTab();
+}
+
+sidebar_links.forEach((link) => link.addEventListener("click", changeLink));
+
+function showTooltip() {
+  let tooltip = this.parentNode.lastElementChild;
+  let spans = tooltip.children;
+  let tooltipIndex = this.dataset.tooltip;
+
+  Array.from(spans).forEach((sp) => sp.classList.remove("show"));
+  spans[tooltipIndex].classList.add("show");
+
+  tooltip.style.top = `${(100 / (spans.length * 2)) * (tooltipIndex * 2 + 1)}%`;
+}
+
+tooltip_elements.forEach((elem) => {
+  elem.addEventListener("mouseover", showTooltip);
+});
+
+// Function to print the result.
+document.getElementById("save-button").addEventListener("click", function () {
+  const pdfContent = document.getElementById("pdf-content"); // Main content to convert
+
+  const pdfOptions = {
+    margin: 10, // PDF margin (optional)
+    filename: "result.pdf", // PDF file name
+    image: { type: "jpeg", quality: 0.98 }, // Image quality (optional)
+    html2canvas: { scale: 2 }, // html2canvas options (optional)
+    jsPDF: { unit: "mm", format: "a4", orientation: "portrait" }, // jsPDF options (optional)
+  };
+
+  html2pdf()
+    .from(pdfContent)
+    .set(pdfOptions)
+    .outputPdf(function (pdf) {
+      const blob = new Blob([pdf], { type: "application/pdf" });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = pdfOptions.filename;
+      a.click();
+    });
+});
